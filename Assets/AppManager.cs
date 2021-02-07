680690b1b546de789abd588e3fa9629dcaf87049
Rei <reisegev@yahoo.com>
2021-02-07 15:55:30 +0200

    added appmanager

---
+++ Assets/AppManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+public class AppManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
