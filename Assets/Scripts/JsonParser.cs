48d8fda463f4d71ffe6e437cc4aacd12edaaa156
elor504 <74613768+elor504@users.noreply.github.com>
2021-02-07 16:29:18 +0200

    json parser class

---
+++ Assets/Scripts/JsonParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonParser 
+{
+    private class PlayerLibrary
+    {
+        int game_count;
+        private class Game
+        {
+            int appid;
+            int playtime_forever;
+            int playtime_windows_forever;
+            int playtime_mac_forever;
+            int playtime_linux_forever;
+        }
+    }
+
+    public void GetPlayerLibraryJson(string Json)
+    {
+        //object JsonTest = JsonUtility.FromJson(Json, Json.GetType());
+    }
+
+
+
+}
+
