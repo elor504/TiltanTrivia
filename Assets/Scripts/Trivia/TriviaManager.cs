9e2dca286a177774b778a79363d220a617f7744d
elor504 <74613768+elor504@users.noreply.github.com>
2021-02-12 13:02:11 +0200

    Question UI
    we can see the question and the answers are randomized and the buttons know which string to return

--- Assets/Scripts/Trivia/TriviaManager.cs
+++ Assets/Scripts/Trivia/TriviaManager.cs
@@ -7,7 +7,22 @@ public class TriviaManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Shuffle(ref array);
+        //Console.WriteLine("SHUFFLE: {0}", string.Join(",", array));
+
+        string[] array2 = { "bird", "frog", "cat","Kaki" };
+        Shuffle(ref array2);
+
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i].ToString());
+        }
+        for (int i = 0; i < array2.Length; i++)
+        {
+            Debug.Log(array2[i].ToString());
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +30,46 @@ public class TriviaManager : MonoBehaviour
     {
         
     }
+
+
+
+    public string[] AnswerRandomizer(string Answer1,string Answer2,string Answer3,string Answer4)
+    {
+        string[] ShuffleCache = new string[4];
+        ShuffleCache[0] = Answer1;
+        ShuffleCache[1] = Answer2;
+        ShuffleCache[2] = Answer3;
+        ShuffleCache[3] = Answer4;
+        Shuffle(ref ShuffleCache);
+
+        return ShuffleCache;
+    }
+
+
+
+    /// Used in Shuffle(T).
+    /// </summary>
+    static System.Random _random = new System.Random();
+
+
+    void Shuffle<T>(ref T[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < (n - 1); i++)
+        {
+            // Use Next on random instance with an argument.
+            // ... The argument is an exclusive bound.
+            //     So we will not go past the end of the array.
+            int r = i + _random.Next(n - i);
+            T t = array[r];
+            array[r] = array[i];
+            array[i] = t;
+        }
+       
+    }
+
+
+
+
+
 }
---
+++ Assets/TriviaUIManager.cs
@@ -0,0 +1,71 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriviaUIManager : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI questionText;
+    [SerializeField]
+    TextMeshProUGUI[] answersText;
+    [SerializeField]
+    Button[] AnswerButts;
+
+
+    private void Start()
+    {
+        
+        UpdateQuestion("Question Test", "This Test Is Stupid", "It Works!", "Stop it, it tickles!", "kaki");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            UpdateQuestion("Question Test", "This Test Is Stupid", "It Works!", "Stop it, it tickles!", "kaki");
+        }
+    }
+
+    public void UpdateQuestion(string Question, string answer1, string answer2, string answer3, string answer4)
+    {
+        RemoveButtonsEvent();
+        questionText.text = Question;
+
+        string[] AnswerCache = GetComponent<TriviaManager>().AnswerRandomizer(answer1, answer2, answer3, answer4);
+
+        UpdateAnswerText(AnswerCache);
+        UpdateButtonsEvent(AnswerCache);
+    }
+
+    void UpdateAnswerText(string[] Answers)
+    {
+        answersText[0].text = Answers[0];
+        answersText[1].text = Answers[1];
+        answersText[2].text = Answers[2];
+        answersText[3].text = Answers[3];
+    }
+
+    void UpdateButtonsEvent(string[] Answer)
+    {
+        AnswerButts[0].onClick.AddListener(delegate { ClickOnAnswer(Answer[0]); });
+        AnswerButts[1].onClick.AddListener(delegate { ClickOnAnswer(Answer[1]); });
+        AnswerButts[2].onClick.AddListener(delegate { ClickOnAnswer(Answer[2]); });
+        AnswerButts[3].onClick.AddListener(delegate { ClickOnAnswer(Answer[3]); });
+    }
+
+    void RemoveButtonsEvent()
+    {
+        for (int i = 0; i < AnswerButts.Length; i++)
+        {
+            AnswerButts[i].onClick.RemoveAllListeners();
+        }
+    }
+
+
+    void ClickOnAnswer(string Answer)
+    {
+        Debug.Log("Test On Buttons: " + Answer);
+    }
+
+
+}
