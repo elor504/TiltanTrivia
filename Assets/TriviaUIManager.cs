71fc22825d768c0f1ab8679a7eb34fad6692ee47
elor504 <74613768+elor504@users.noreply.github.com>
2021-02-12 16:30:45 +0200

    login scene 90% logic

--- Assets/TriviaUIManager.cs
+++ Assets/TriviaUIManager.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,9 +16,11 @@ public class TriviaUIManager : MonoBehaviour
     [SerializeField]
     float TimerTest;
 
+    public LoginUI loginUI;
+
     private void Start()
     {
-        
+
         UpdateQuestion("Question Test", "This Test Is Stupid", "It Works!", "Stop it, it tickles!", "kaki");
     }
 
@@ -28,14 +31,30 @@ public class TriviaUIManager : MonoBehaviour
             UpdateQuestion("Question Test", "This Test Is Stupid", "It Works!", "Stop it, it tickles!", "kaki");
         }
 
-        TimerTest -= Time.deltaTime;
-        if (TimerTest > 0)
+
+        if (TimerTest >= 0)
         {
+            TimerTest -= Time.deltaTime;
             UpdateTimerUI(TimerTest);
         }
-       
+
 
     }
+
+
+    #region TriviaUI
+    [SerializeField]
+    GameObject TriviaUIGO;
+    public void OpenTriviaUI()
+    {
+        TriviaUIGO.SetActive(true);
+    }
+
+    public void CloseTriviaUI()
+    {
+        TriviaUIGO.SetActive(false);
+    }
+
 
     public void UpdateQuestion(string Question, string answer1, string answer2, string answer3, string answer4)
     {
@@ -46,6 +65,7 @@ public class TriviaUIManager : MonoBehaviour
 
         UpdateAnswerText(AnswerCache);
         UpdateButtonsEvent(AnswerCache);
+
     }
 
     void UpdateAnswerText(string[] Answers)
@@ -82,6 +102,202 @@ public class TriviaUIManager : MonoBehaviour
     public void UpdateTimerUI(float Time)
     {
         timerText.text = Mathf.CeilToInt(Time).ToString();
+        if (Time < 5)
+        {
+
+        }
+    }
+
+
+    #endregion
+
+
+    #region TriviaLogin
+    [SerializeField]
+    GameObject loginUIGO;
+    [SerializeField]
+    GameObject PasswordPanel;
+
+    public void OpenLoginUI()
+    {
+        loginUIGO.SetActive(true);
+    }
+
+    public void CloseLoginUI()
+    {
+        loginUIGO.SetActive(false);
+    }
+
+    public void OnChangeNameInput()
+    {
+        loginUI.GetPlayerName(loginUI.getPlayerNameInput.text);
+    }
+
+    public void OnChangeRoomId()
+    {
+
+
+        if (loginUI.getRoomIdInput.text.Length != 0)
+        {
+            loginUI.GetRoomId(int.Parse(loginUI.getRoomIdInput.text));
+        }
+        else
+        {
+            loginUI.GetRoomId(0);
+        }
+
+
+    }
+        #endregion
+    public void OnChangePasswordInput()
+    {
+        loginUI.OnChangePassword();
+    }
+    public void OnChangeJoiningPasswordInput()
+    {
+        loginUI.OnChangeRoomJoinPassword();
+    }
+
+
+    public void OnClickHasPasswordToggle()
+    {
+        loginUI.ToggleHasPassword();
+    }
+
+
+
+    public void OnClickRandomMatch()
+    {
+        Debug.Log("Searching for random game");
+    }
+
+    public void OnClickCreateRoom()
+    {
+        if (loginUI.getHasPasswordToggle.isOn)
+        {
+            Debug.Log("Creating room with password");
+        }
+        else
+        {
+            Debug.Log("Creating room without password");
+        }
+      
+    }
+
+    public void OnClickJoinRoomWithId()
+    {
+        Debug.Log("Joining Room With Id that has no password");
+        //add functionality that see if room ask for password if it does it will pop up a panel with password
+        if (true)
+        {
+            PasswordPanel.SetActive(true);
+        }
+
+    }
+
+    public void ClosePasswordPanel()
+    {
+        PasswordPanel.SetActive(false);
+    }
+
+
+    public void OnClickJoinRoomWithPassword()
+    {
+
+    }
+
+    }
+
+[Serializable]
+public class LoginUI
+{
+    [Header("Login Related")]
+    [SerializeField]
+    TMP_InputField playerNameInput;
+    public TMP_InputField getPlayerNameInput => playerNameInput;
+
+    [SerializeField]
+    string thisPlayerName;
+
+    [SerializeField]
+    bool isRoomPrivate;
+
+
+    [SerializeField]
+    TMP_InputField roomIdInput;
+    public TMP_InputField getRoomIdInput => roomIdInput;
+
+    [SerializeField]
+    TMP_InputField passwordInput;
+
+    [SerializeField]
+    TMP_InputField roomPasswordInput;
+
+    [SerializeField]
+    string roomPassword;
+    [SerializeField]
+    string joiningRoomPassword;
+    public string getJoiningRoomPassword => joiningRoomPassword;
+    [SerializeField]
+    int searchRoomId;
+    public int getSearchRoomId => searchRoomId;
+
+
+
+    [SerializeField]
+    Toggle hasPaswordToggle;
+    public Toggle getHasPasswordToggle => hasPaswordToggle;
+
+
+
+
+
+    public void CreateRoom()
+    {
+        Debug.Log("Creating Room");
+    }
+
+    public void GetRoomId(int Id)
+    {
+        searchRoomId = Id;
+    }
+
+    public void GetPlayerName(string Name)
+    {
+        thisPlayerName = Name;
+    }
+
+    public void ToggleHasPassword()
+    {
+        isRoomPrivate = hasPaswordToggle.isOn;
+
+        passwordInput.interactable = hasPaswordToggle.isOn;
+
+    }
+
+    public void OnChangePassword()
+    {
+        if(passwordInput.text.Length != 0)
+        {
+            roomPassword = passwordInput.text;
+        }
+        else
+        {
+            roomPassword = null;
+        }
+       
+    }
+
+    public void OnChangeRoomJoinPassword()
+    {
+        if (roomPasswordInput.text.Length != 0)
+        {
+            joiningRoomPassword = roomPasswordInput.text;
+        }
+        else
+        {
+            joiningRoomPassword = null;
+        }
     }
 
 
