c2b615c36c084bfca935d8f04866ee84609f14b3
Rei <reisegev@yahoo.com>
2021-02-12 13:17:59 +0200

    Update ConnectToDB.cs

--- Assets/Scripts/Trivia/ConnectToDB.cs
+++ Assets/Scripts/Trivia/ConnectToDB.cs
@@ -1,38 +1,28 @@
 ﻿
+using NUnit.Framework.Constraints;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class ConnectToDB : MonoBehaviour
 {
-    string db_IP = "80.230.92.74:3306";
-    string db_PW = "elor2020";
+    //string db_IP = "80.230.92.74:3306";
+    //string db_PW = "elor2020";
+   // int id = 1;
+    //string db_server = "trivia";
+    
+    string connStr = "https://localhost:44306/api/Question/1";
 
-    string db_server = "trivia";
-    //  mysql --host=localhost --user=myname --password=password mydb
-    int id = 1;
-        string connStr = "http://localhost:44306/api/Question/";
-    string selectAll = "Select * From testTable";
-    IEnumerator SendRequest(string url) {
-        UnityWebRequest webReq = UnityWebRequest.Get(url);
-             
-        yield return webReq.SendWebRequest();
-
-        if (webReq.isNetworkError || webReq.isHttpError)
-        {
-            Debug.Log(webReq.error);
-        }
-        else
-        {
-            Debug.Log("ConnectionAcksType");
-        
-
-        }
+    string jsonData;
+    void SetJsonData(string json)
+    {
+        jsonData = json;
+        Debug.Log(jsonData);
     }
     private void Start()
     {
-        StartCoroutine(SendRequest(connStr));
+        StartCoroutine(WebFetch.ConnectToAPI(connStr, SetJsonData));
+        Debug.Log(jsonData);
     }
-
 
 }
