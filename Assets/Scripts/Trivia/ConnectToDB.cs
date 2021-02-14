b31da9c6976e7e82e7385e3fe3c9f30d6c1cbb09
Rei <reisegev@yahoo.com>
2021-02-14 22:42:14 +0200

    start

--- Assets/Scripts/Trivia/ConnectToDB.cs
+++ Assets/Scripts/Trivia/ConnectToDB.cs
@@ -1,5 +1,7 @@
 ﻿
+using JetBrains.Annotations;
 using NUnit.Framework.Constraints;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,9 +10,9 @@ public class ConnectToDB : MonoBehaviour
 {
     //string db_IP = "80.230.92.74:3306";
     //string db_PW = "elor2020";
-   // int id = 1;
+    // int id = 1;
     //string db_server = "trivia";
-    
+
     string connStr = "https://localhost:44306/api/Question/1";
 
     string jsonData;
@@ -19,10 +21,57 @@ public class ConnectToDB : MonoBehaviour
         jsonData = json;
         Debug.Log(jsonData);
     }
+
+
+    void TryAddToInventory(string Done ) {
+        Debug.Log(Done);
+
+
+    }
     private void Start()
     {
-        StartCoroutine(WebFetch.ConnectToAPI(connStr, SetJsonData));
-        Debug.Log(jsonData);
+
+
+        var QuestionData = new QuestionLibrary();
+
+        QuestionData.qd.QuestionId = "20";
+        QuestionData.qd.Question = "TRY";
+        QuestionData.qd.CorrectAnswer = "TRY";
+        QuestionData.qd.Answer1 = "TRY";
+        QuestionData.qd.Answer2 = "TRY";
+        QuestionData.qd.Answer3 = "TRY";
+        string obj = JsonUtility.ToJson(QuestionData.qd);
+
+
+
+        string uri = "https://localhost:44306/api/Question";
+        Debug.Log(obj);
+
+        StartCoroutine(WebFetch.HttpPost(uri, obj, TryAddToInventory));
     }
 
+    [Serializable]
+    public class QuestionLibrary
+    {
+
+        public QuestionLibrary()
+        {
+            qd = new QuestionData();
+        }
+        public QuestionData qd;
+
+
+
+        [Serializable]
+        public class QuestionData
+        {
+            public string QuestionId;
+            public string Question;
+            public string CorrectAnswer;
+            public string Answer1;
+            public string Answer2;
+            public string Answer3;
+        }
+
+    }
 }
--- Assets/Scripts/WebFetch.cs
+++ Assets/Scripts/WebFetch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.IO;
+using System.Net;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -59,5 +60,91 @@ public static class WebFetch
             }
         }
     }
+    public static IEnumerator HttpGet(string uri, Action<string> callback = null) {
+        using (UnityWebRequest webReq = UnityWebRequest.Get(uri))
+        {
 
+            yield return webReq.SendWebRequest();
+
+            if (webReq.isNetworkError)
+            {
+                Debug.Log("Error");
+            }
+            else
+            {
+                string data = Encoding.UTF8.GetString(webReq.downloadHandler.data);
+                Debug.Log(data);
+                Debug.Log("Recieved!");
+                callback?.Invoke(data);
+            }
+
+        }
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private const string defaultContentType = "application/json";
+    public static IEnumerator HttpPost(string uri, string jsonBody, Action<string> callback = null) {
+
+        using (UnityWebRequest webReq = UnityWebRequest.Post(uri, jsonBody))
+        {
+            webReq.SetRequestHeader("Content-Type", defaultContentType);
+            webReq.uploadHandler.contentType = defaultContentType;
+            webReq.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(jsonBody));
+  
+          yield return webReq.SendWebRequest();
+
+            if (webReq.isNetworkError)
+            {
+                callback?.Invoke("Error");
+            }
+            else
+            {
+                //string data = System.Text.Encoding.UTF8.GetString(webReq.downloadHandler.data);
+                //Debug.Log(data);
+
+                callback?.Invoke("Send");
+            }
+
+        }
+    
+    
+    }
 }
